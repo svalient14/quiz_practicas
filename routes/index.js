5f33894d46c18a1d055348cc7e33c4a2269b0616
svalient14 <sara.valiente.merino@alumnos.upm.es>
2016-06-02 15:59:19 +0200

    Tema 3: Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+//nueva variable
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
 module.exports = router;
+
+/* GET question. */
+router.get('/question',quizController.question);
+
+/* GET check. */
+router.get('/check',quizController.check);
