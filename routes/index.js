d846adb0e024096aa8d94558bbd9e57b32aa60c9
svalient14 <sara.valiente.merino@alumnos.upm.es>
2016-06-02 16:25:24 +0200

    Tema 5: Marco de la aplicacion

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index');
 });
 
 module.exports = router;
